f4e6311d5d6964be10e6950aeadca48ea078406e
BnnQ <1v25fa94@gmail.com>
2023-05-15 19:54:22 +0300

    Added Azure section configuration projection class

---
+++ Configuration/Azure.cs
@@ -0,0 +1,23 @@
+namespace CarManager.Configuration;
+
+public class Azure
+{
+    public Cosmos? Cosmos { get; set; }
+}
+
+public class Cosmos
+{
+    public CarsDatabase? CarsDatabase { get; set; }
+}
+
+public class CarsDatabase
+{
+    public string? Id { get; set; }
+    public CarsContainer? CarsContainer { get; set; }
+}
+
+public class CarsContainer
+{
+    public string? Id { get; set; }
+    public string? PartitionKeyValue { get; set; }
+}
