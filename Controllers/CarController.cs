5f4cb910226603be44733c204f47173743f04dd8
BnnQ <1v25fa94@gmail.com>
2023-05-16 19:20:02 +0300

    Global refactoring of CarController, data manipulation logic delegated to IRepository

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,70 +1,39 @@
-using System.Net;
-using System.Text.Json;
 using AutoMapper;
 using CarManager.Models;
 using CarManager.Models.Dto;
-using CarManager.Services;
 using CarManager.Services.Abstractions;
-using CarManager.Utils.Extensions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Options;
 
 namespace CarManager.Controllers;
 
-public class CarController : Controller
+[Route(template: "api/[controller]")]
+[ApiController]
+public class CarController : ControllerBase
 {
-    private readonly CosmosAccountClient cosmosClient;
-    private readonly JsonSerializerOptions serializerOptions;
-    private readonly IMapper mapper;
+    private readonly IRepository<Car, string> carRepository;
     private readonly ILogger<CarController> logger;
+    private readonly IMapper mapper;
 
-    private readonly string databaseId;
-    private readonly string containerId;
-    private readonly string partitionKeyPath;
-    private readonly PartitionKey partitionKey;
-
-    public CarController(ILoggerFactory loggerFactory,
-        CosmosAccountClient cosmosClient,
-        IOptions<Configuration.Azure> azureOptions,
-        JsonSerializerOptions serializerOptions,
-        IMapper mapper)
+    public CarController(IRepository<Car, string> carRepository, IMapper mapper, ILoggerFactory loggerFactory)
     {
-        this.cosmosClient = cosmosClient;
-        this.serializerOptions = serializerOptions;
+        this.carRepository = carRepository;
         this.mapper = mapper;
         logger = loggerFactory.CreateLogger<CarController>();
-
-        databaseId = azureOptions.Value.Cosmos?.CarsDatabase?.Id ??
-                     throw new InvalidOperationException("'Azure.Cosmos.CarsDatabase.Id' configuration value is not provided");
-
-        containerId = azureOptions.Value.Cosmos.CarsDatabase.CarsContainer?.Id ??
-                      throw new InvalidOperationException(
-                          "'Azure.Cosmos.CarsDatabase.CarsContainer.Id' configuration value is not provided");
-
-        partitionKeyPath = "/" + (azureOptions.Value.Cosmos.CarsDatabase.CarsContainer.PartitionKeyValue ??
-                                  throw new InvalidOperationException(
-                                      "'Azure.Cosmos.CarsDatabase.CarsContainer.PartitionKeyValue' configuration value is not provided"));
-
-        partitionKey = new PartitionKey(azureOptions.Value.Cosmos.CarsDatabase.CarsContainer.PartitionKeyValue);
     }
 
-    [HttpGet(Name = "api")]
-    public async Task<IActionResult> GetCars()
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Car>>> GetCars()
     {
-        var container = await cosmosClient.GetOrCreateContainerAsync(databaseId, containerId, partitionKeyPath);
-        var cars = await container.ExecuteQueryAsync<Car>();
+        var cars = await carRepository.GetItemsAsync();
 
         logger.LogInformation("[GET] GetCars: returning JSON cars from database");
-        return Json(data: cars, serializerSettings: serializerOptions);
+        return cars.ToList();
     }
 
-    [HttpGet(Name = "api")]
-    public async Task<IActionResult> GetCar(string id)
+    [HttpGet(template: "{id}")]
+    public async Task<ActionResult<Car>> GetCar(string id)
     {
-        var container = await cosmosClient.GetOrCreateContainerAsync(databaseId, containerId, partitionKeyPath);
-        var car = (await container.ExecuteQueryAsync<Car>(car => car.Id.Equals(id))).SingleOrDefault();
-
+        var car = await carRepository.GetItemAsync(id);
         if (car is null)
         {
             logger.LogWarning("[GET] GetCar: car with ID '{CarId}' not found, returning 404 Not Found", id);
@@ -72,47 +41,44 @@ public class CarController : Controller
         }
 
         logger.LogInformation("[GET] GetCar: returning JSON car with ID '{CarId}' from database", id);
-        return Json(data: car, serializerSettings: serializerOptions);
+        return car;
     }
 
-    [HttpPost(Name = "api")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> CreateCar(CarDto carDto, [FromServices] IIdentifierGenerator identifierGenerator)
+    [HttpPost]
+    public async Task<ObjectResult> PostCar(CarDto carDto, [FromServices] IIdentifierGenerator identifierGenerator)
     {
         if (!ModelState.IsValid)
         {
-            logger.LogWarning("[POST] CreateCar: model is not valid, returning 400 Bad Request");
+            logger.LogWarning("[POST] PostCar: model is not valid, returning 400 Bad Request");
             return BadRequest(ModelState);
         }
-        
+
         var car = mapper.Map<Car>(carDto);
         car.Id = identifierGenerator.GetNextIdentifier();
 
-        var container = await cosmosClient.GetOrCreateContainerAsync(databaseId, containerId, partitionKeyPath);
-        await container.CreateItemAsync(car, partitionKey);
-
-        logger.LogInformation("[POST] CreateCar: sucessfully created car with ID '{CarId}' returning JSON of created car", car.Id);
+        await carRepository.AddItemsAsync(car);
+        logger.LogInformation("[POST] PostCar: sucessfully created car with ID '{CarId}', returning JSON of created car", car.Id);
         return CreatedAtAction(value: car, actionName: nameof(GetCar), routeValues: new { id = car.Id });
     }
 
-    [HttpPut(Name = "api")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> EditCar(Car car)
+    [HttpPut(template: "{id}")]
+    public async Task<IActionResult> PutCar(string id, CarDto carDto)
     {
         if (!ModelState.IsValid)
         {
-            logger.LogWarning("[PUT] CreateCar: model is not valid, returning 400 Bad Request");
+            logger.LogWarning("[PUT] PostCar: model is not valid, returning 400 Bad Request");
             return BadRequest(ModelState);
         }
-        
-        var container = await cosmosClient.GetOrCreateContainerAsync(databaseId, containerId, partitionKeyPath);
-        await container.ReplaceItemAsync(car, car.Id, partitionKey);
-        
-        logger.LogInformation("[PUT] EditCar: sucessfully edited car with ID '{CarId}', returning 200 OK", car.Id);
-        return Ok();
+
+        var car = mapper.Map<Car>(carDto);
+        car.Id = id;
+
+        await carRepository.EditItemAsync(car.Id, car);
+        logger.LogInformation("[PUT] PutCar: sucessfully edited car with ID '{CarId}', returning 200 OK", car.Id);
+        return NoContent();
     }
 
-    [HttpDelete(Name = "api")]
+    [HttpDelete(template: "{id}")]
     public async Task<IActionResult> DeleteCar(string id)
     {
         if (string.IsNullOrWhiteSpace(id))
@@ -121,17 +87,19 @@ public class CarController : Controller
             return BadRequest("Passed 'id' is not valid");
         }
 
-        var container = await cosmosClient.GetOrCreateContainerAsync(databaseId, containerId, partitionKeyPath);
-        var response = await container.DeleteItemAsync<Car>(id, partitionKey);
-
-        if (response.StatusCode is not HttpStatusCode.OK)
+        try
         {
-            logger.LogWarning("[DELETE] DeleteCar: car with passed ID {CarId} not found, returning 404 Not Found", id);
+            await carRepository.DeleteItemAsync(id);
+        }
+        catch (Exception exception)
+        {
+            logger.LogWarning(
+                "[DELETE] DeleteCar: error occurred when deleting car with id '{CarId}' ({ErrorMessage}), returning 404 Not Found", id,
+                exception.Message);
             return NotFound();
         }
-        
+
         logger.LogInformation("[DELETE] DeleteCar: sucessfully deleted car with ID '{CarId}', returning 204 No Content", id);
         return NoContent();
     }
-
 }
