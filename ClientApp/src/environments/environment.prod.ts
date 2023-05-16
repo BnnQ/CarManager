080ba6d3d80bc8eb975c94096468faed0bea0393
BnnQ <1v25fa94@gmail.com>
2023-05-16 19:20:57 +0300

    Added environment variables

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  carApi: {
+    host: 'localhost',
+    port: '7173',
+    route: 'api/Car'
+  },
 };
--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  carApi: {
+    host: 'localhost',
+    port: '7173',
+    route: 'api/Car'
+  },
 };
 
 /*
